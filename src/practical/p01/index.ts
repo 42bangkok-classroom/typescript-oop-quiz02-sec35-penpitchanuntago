eae4cd35162032bc5dcd149813470300fcefe7aa
penpitchanuntago <68010826@kmitl.ac.th>
2026-01-16 10:04:13 +0700

    commit choice

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,8 @@
 export function getEdgePosts() {}
+{
+  const User = fetch ('https://jsonplaceholder.typicode.com/posts')
+    userId: number,
+    "id": number,
+    "title": string,
+    "body: string"
+  }
