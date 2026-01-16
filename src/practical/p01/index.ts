033d6193d6463c57016e443ec46d40f4daba7137
penpitchanuntago <68010826@kmitl.ac.th>
2026-01-16 10:25:32 +0700

    p01 commit

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,8 +1,17 @@
+interface User1 {
+    userId : number,
+    id: number,
+    title: string,
+    body: string
+  } 
+  const fetchUsers = async (): Promise<User1[]> => {
+const User = fetch ('https://jsonplaceholder.typicode.com/posts')
+const data : User1[] = await (await User).json(); 
+
+return data ;
+}
 export function getEdgePosts() {}
 {
-  const User = fetch ('https://jsonplaceholder.typicode.com/posts')
-    userId: number,
-    "id": number,
-    "title": string,
-    "body: string"
+ 
+  
   }
